d057740d08cd0c47635cd2044990e21e1697de83
Mikhail <mikhail.belikov@airbapay.kz>
2023-07-18 10:43:37 +0600

    test

--- mylibrary/src/main/java/kz/airbapaytest/mylibrary/WitTest.kt
+++ mylibrary/src/main/java/kz/airbapaytest/mylibrary/WitTest.kt
@@ -1,9 +1,15 @@
-package kz.airbapay.mylibrary
+package kz.airbapaytest.mylibrary
 
 import android.util.Log
 
 object WitTest {
     fun initTest() {
+        Log.v("aaaaaa", "eeeeee")
+    }
+}
+
+class Wiit {
+    fun initTest1() {
         Log.v("aaaaaa", "eeeeee")
     }
 }
